aeb21294957a9bc7ca22d1e281104c6133aeac27
Ritika Mishra <mishra.r.0804@gmail.com>
2023-12-11 09:15:27 +0530

    Two pointer approach

--- MostWater.java
+++ MostWater.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+ //brute force approach On2
 public class MostWater {
     public static int mostWaterIs(ArrayList<Integer>height){
         int maxWater = 0 ;
@@ -14,8 +14,31 @@ public class MostWater {
 
         return maxWater;
     }
+
+    //two pointer approach->lp & rp
+
+    public static int mostWater(ArrayList<Integer>height){
+        int max = 0;
+            int lp = 0;
+            int rp = height.size()-1;
+            while(lp<rp){
+              int width = rp-lp;
+              int length = Math.min(height.get(lp),height.get(lp));
+              max=Math.max(max,length*width);
+
+              if(height.get(lp)<height.get(rp)){
+                lp++;
+               }
+               else{
+                rp--;
+               }
+            }
+        return max;
+    }
+    
+
     public static void main(String[] args) {
-        ArrayList height = new ArrayList<>();
+        ArrayList<Integer> height = new ArrayList<>();
         height.add(1);
         height.add(7);
         height.add(8);
@@ -23,6 +46,6 @@ public class MostWater {
         height.add(4);
         height.add(0);
 
-        System.out.println(mostWaterIs(height));
+        System.out.println(mostWater(height));
     }
 }
